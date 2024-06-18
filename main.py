918ee8a92bee820d85e76653da84960e7a390a35
ramses2099 <ramses2099@gmail.com>
2024-06-18 10:37:19 -0400

    last commit

--- main.py
+++ main.py
@@ -16,6 +16,18 @@ def main():
         
         retrieved_doc = db[doc_id]
         print('Retrieved document: {0}'.format(retrieved_doc))
+        
+        # Update a field in the document
+        retrieved_doc['age'] = 31
+        
+        # Save the updated document back to the database
+        db.save(retrieved_doc)
+        print('Updated document: {0}'.format(retrieved_doc))
+        
+        # Delete the document
+        db.delete(retrieved_doc)
+        print('Deleted document with ID: {0}'.format(retrieved_doc['_id']))
+        
 
     except Exception as e:
         raise print(e)
